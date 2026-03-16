3e654ceb3c05e80826e0611391a1a0bf840ee6d2
aditishinde9585 <aditishinde9585@gmail.com>
2026-03-16 17:04:03 +0530

    added vercel frontend url in cors

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  server: {
+    port: 5173
+  },
+
+  define: {
+    __API_URL__: JSON.stringify("https://server-le4u.onrender.com")
+  }
+
+});
